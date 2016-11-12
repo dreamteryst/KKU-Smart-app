55d8d6172d4969f0a237be0f680e1c4f8ca50c75
Kittisak Pimnonthong <dreamt451@gmail.com>
2016-11-12 14:17:49 +0700

    sign up check space

--- app/src/main/java/com/dreamdev/kkusmartapp/SignUpActivity.java
+++ app/src/main/java/com/dreamdev/kkusmartapp/SignUpActivity.java
@@ -2,12 +2,52 @@ package com.dreamdev.kkusmartapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button button;
+    private EditText nameEditText, phoneEditText, userEditText, passwordEditText;
+    private ImageView imageView;
+    private String nameString, phoneString, userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        phoneEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Get Value Form EditText
+                nameString = nameEditText.getText().toString().trim();
+                phoneString = phoneEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.isEmpty() || phoneString.isEmpty() || userString.isEmpty() ||passwordString.isEmpty()) {
+                    //Have space
+                    Log.d("Onclick", "Have Space");
+                }// IF IsEmpty
+
+            }// Onclick
+        });
+
+    }// Main Method
+
+}// Main Classs
