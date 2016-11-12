c7de21ab0641a2b8403c631b90e66a1456fc17bc
Kittisak Pimnonthong <dreamt451@gmail.com>
2016-11-12 16:43:02 +0700

    Finish 12Nov

--- app/src/main/java/com/dreamdev/kkusmartapp/SignUpActivity.java
+++ app/src/main/java/com/dreamdev/kkusmartapp/SignUpActivity.java
@@ -1,6 +1,12 @@
 package com.dreamdev.kkusmartapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +15,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
+
 public class SignUpActivity extends AppCompatActivity {
 
     private static final String TAG = "SignUpActivity";
@@ -16,7 +26,9 @@ public class SignUpActivity extends AppCompatActivity {
     private Button button;
     private EditText nameEditText, phoneEditText, userEditText, passwordEditText;
     private ImageView imageView;
-    private String nameString, phoneString, userString, passwordString;
+    private String nameString, phoneString, userString, passwordString, imagePathString, imageNameString;
+    private Uri uri;
+    private Boolean aBoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,7 @@ public class SignUpActivity extends AppCompatActivity {
         passwordEditText = (EditText) findViewById(R.id.editText4);
         imageView = (ImageView) findViewById(R.id.imageView);
         button = (Button) findViewById(R.id.button);
+        aBoolean = true;
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +59,14 @@ public class SignUpActivity extends AppCompatActivity {
                     //Have space
                     MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.bird48, "Have a space", "กรุณากรอกข้อความให้ครบทุกช่อง");
                     myAlert.myDialog();
-                }// IF IsEmpty
+                } else if (aBoolean) {
+                    //Non Choose
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48, "ยังไม่เลือกรูป", "กรุณาเลือกรูปด้วยนะครับ");
+                    myAlert.myDialog();
+                } else {
+                    //Choose Image OK
+                    uploadImageToServer();
+                }
 
             }// Onclick
         });
@@ -64,16 +84,76 @@ public class SignUpActivity extends AppCompatActivity {
 
     }// Main Method
 
+    private void uploadImageToServer() {
+
+        //Change policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect("ftp.swiftcodingthai.com", 21, "kku@swiftcodingthai.com", "Abc12345");
+            simpleFTP.bin();
+            simpleFTP.cwd("Image");
+            simpleFTP.stor(new File(imagePathString));
+            simpleFTP.disconnect();
+
+        } catch (Exception e) {
+            Log.d(TAG, "uploadImageToServer: " + e.toString());
+        }
+
+    }// Upload
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if ((requestCode == 0) && (resultCode == RESULT_OK)) {
 
+            aBoolean = false;
+
             Log.d(TAG, "onActivityResult: SelectImage!");
+
+            //Show image
+            uri = data.getData();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //Find Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d(TAG, "onActivityResult: Path = " + imagePathString);
+
+            //Find Name of Image
+            imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
+            Log.d(TAG, "onActivityResult: ImageName = " + imageNameString);
+
 
         } // if
 
     }// onActivity
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
+        if (cursor != null) {
+
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(index);
+
+        } else {
+            result = uri.getPath();
+        }
+
+        return result;
+    }
 
 }// Main Classs
