de98538e9de9260a90e637818d78fea9e082ea10
Kittisak Pimnonthong <dreamt451@gmail.com>
2016-11-13 14:29:04 +0700

    Confirm Call

--- app/src/main/java/com/dreamdev/kkusmartapp/ServiceActivity.java
+++ app/src/main/java/com/dreamdev/kkusmartapp/ServiceActivity.java
@@ -2,12 +2,44 @@ package com.dreamdev.kkusmartapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private String[] nameStrings, phoneStrings, imageStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.livFriend);
+
+        //Receive Value From Intent
+        nameStrings = getIntent().getStringArrayExtra("Name");
+        phoneStrings = getIntent().getStringArrayExtra("Phone");
+        imageStrings = getIntent().getStringArrayExtra("Image");
+
+        //Create ListView
+        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, nameStrings, phoneStrings, imageStrings);
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                confirmCall(nameStrings[position], phoneStrings[position]);
+            }// On Item Click
+        });
+
+    }// Main Method
+
+    private void confirmCall(String nameString, String phoneString) {
+
     }
-}
+
+}// Main class
